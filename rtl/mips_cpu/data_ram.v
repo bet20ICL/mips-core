97bda79e6dceeba657e57f46b3a7e3230f518e48
kiril <ka1020@ic.ac.uk>
2021-11-25 15:13:12 +0000

    updated ram

--- rtl/mips_cpu/data_ram.v
+++ rtl/mips_cpu/data_ram.v
@@ -8,5 +8,22 @@ module data_ram (
     input logic[31:0]  data_writedata,
     output logic[31:0]  data_readdata
 );
+
+
+
+    reg [31:0] ram [65535:0];
+    integer i;
+    
+    initial begin
+        for(i=0;i<65535;i=i+1)
+            ram[i] <= 32'd0;
+    end
+    always @(posedge clk) begin
+        if (data_write)
+            ram[adress] <= data_writedata;
+    end
+    assign data_readdata = ram[data_address];
+endmodule
     
 endmodule
+
