4610b039f741ce3099e2c12eac6da98dc190f159
lucjones02 <luc.xav.jones@gmail.com>
2021-12-02 19:04:03 +0000

    added another shell script

---
+++ test/run_one_instr.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eou pipefail
+
+FILENAME="$1"
+INSTRUCTION="$2"
+
+INTERNAL_FILES_1="${FILENAME}/mips_cpu/*.v"
+INTERNAL_FILES_2="${FILENAME}/mips_cpu_*.v"
+
+TESTBENCHES="test/${INSTRUCTION}*_harvard_tb.v"
+
+
+for i in ${TESTBENCHES}
+do
+    set +e
+    iverilog -Wall -g2012 -o test/t  ${i} ${INTERNAL_FILES_1} ${INTERNAL_FILES_2} 2> test/dump.log
+    RESULT=$?
+    set -e
+
+    if [[ RESULT -ne 0 ]] ; then
+        echo "$(basename ${i} _harvard_tb.v) ${INSTRUCTION} Fail     Failed to compile"
+    else
+        set +e
+        ./test/t > test/$(basename ${i} .v).log
+        OUTPUT=$?
+        set -e
+
+        if [[ RESULT -eq 0 ]] ; then
+            echo "$(basename ${i} _harvard_tb.v) ${INSTRUCITON} Pass"
+        elif [[ RESULT -eq 1 ]] ; then
+            echo ""
+        else 
+            echo ""
+        fi
+    fi
+done
--- test/test_mips_cpu_harvard.sh
+++ test/test_mips_cpu_harvard.sh
@@ -2,43 +2,13 @@
 
 set -eou pipefail
 
-FILENAME="$1"
-args=("$@")
-
-INTERNAL_FILES_1="${FILENAME}/mips_cpu/*.v"
-INTERNAL_FILES_2="${FILENAME}/mips_cpu_*.v"
+FILES="$1"
+ARGS="$@"
 
 if [ -z ${args[1]+x} ]
 then
-    TESTBENCHES="test/*_tb.v"
-    INSTRUCITON=""
+    echo "empty"
+    INSTRUCTION=""
 else
-    TESTBENCHES="test/${args[1]}_*.v"
-    INSTRUCTION="$2"
+    INSTRUCITON
 fi
-
-
-for i in ${TESTBENCHES}
-do
-    set +e
-    iverilog -Wall -g2012 -o test/t  ${i} ${INTERNAL_FILES_1} ${INTERNAL_FILES_2} 2> test/dump.log
-    RESULT=$?
-    set -e
-
-    if [[ RESULT -ne 0 ]] ; then
-        echo "$(basename ${i} _harvard_tb.v) ${INSTRUCTION} Fail     Failed to compile"
-    else
-        set +e
-        ./test/t > test/$(basename ${i} .v).log
-        OUTPUT=$?
-        set -e
-
-        if [[ RESULT -eq 0 ]] ; then
-            echo "$(basename ${i} _harvard_tb.v) ${INSTRUCITON} Pass"
-        elif [[ RESULT -eq 1 ]] ; then
-            echo ""
-        else 
-            echo ""
-        fi
-    fi
-done
