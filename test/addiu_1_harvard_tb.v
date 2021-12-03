360f645049172a4a00c2ec61da124c6bf3038db0
Nicholas Mytilineos <nicholas.mytilineos@hotmail.com>
2021-12-03 12:29:30 +0000

    hope

---
+++ test/addiu_1_harvard_tb.v
@@ -0,0 +1,69 @@
+module addiu_tb();
+
+    logic     clk;
+    logic     reset;
+    logic    active;
+    logic[31:0] register_v0;
+
+    /* New clock enable. See below. */
+    logic     clk_enable;
+
+    /* Combinatorial read access to instructions */
+    logic[31:0]  instr_address;
+    logic[31:0]  instr_readdata;
+
+    /* Combinatorial read and single-cycle write access to instructions */
+    logic[31:0]  data_address, data_address_wanted;
+    logic        data_write;
+    logic        data_read;
+    logic[31:0]  data_writedata;
+    logic[31:0]  data_readdata;
+
+    /* memory initialisation */
+    logic init_mem, instr_active;
+    logic[31:0] init_mem_addr;
+    logic[31:0] init_instr;
+
+    initial begin
+        clk = 0;
+        #1;
+        repeat (1000) begin
+            clk = ~clk;
+            #1;
+        end
+    end
+
+    initial begin
+        
+        #1;
+        instr_readdata = 32'b10001100000011000000000000000000;
+        data_readdata = 32'd4;
+        #2;
+        /* addiu: $3 = $12 + 100 */
+        instr_readdata = 32'b00100101100000110000000001101000;
+        #2;
+        /*important to get time delay correct, so it checks everything done in correct amount of clock cycles */
+        instr_readdata = 32'b10101100000000110000000000000000;
+        
+
+        assert(data_writedata==32'd104) else $fatal(1, "expected output=7, got output=%d",data_writedata);
+
+    end
+
+
+    mips_cpu_harvard dut(
+        .clk(clk),
+        .reset(reset),
+        .active(active),
+        .register_v0(register_v0),
+        .clk_enable(clk_enable),
+        .instr_address(instr_address),
+        .instr_readdata(instr_readdata),
+        .data_address(data_address),
+        .data_write(data_write),
+        .data_read(data_read),
+        .data_writedata(data_writedata),
+        .data_readdata(data_readdata)
+    );
+
+endmodule
---
+++ test/lw_1_harvard_tb.v
@@ -0,0 +1,70 @@
+module lw_tb();
+
+    logic     clk;
+    logic     reset;
+    logic    active;
+    logic[31:0] register_v0;
+
+    /* New clock enable. See below. */
+    logic     clk_enable;
+
+    /* Combinatorial read access to instructions */
+    logic[31:0]  instr_address;
+    logic[31:0]  instr_readdata;
+
+    /* Combinatorial read and single-cycle write access to instructions */
+    logic[31:0]  data_address, data_address_wanted;
+    logic        data_write;
+    logic        data_read;
+    logic[31:0]  data_writedata;
+    logic[31:0]  data_readdata;
+
+    /* memory initialisation */
+    logic init_mem, instr_active;
+    logic[31:0] init_mem_addr;
+    logic[31:0] init_instr;
+
+    initial begin
+        clk = 0;
+        #1;
+        repeat (1000) begin
+            clk = ~clk;
+            #1;
+        end
+    end
+
+    initial begin
+        
+        #1;
+        /* $8=mem[$0+100] */
+        instr_readdata = 32'b10000000110010000000000001101000;
+        data_readdata = 32'd9;
+
+        assert(!data_write) else $fatal(1, "data_write should not be active but is");
+        assert(data_read) else $fatal(1, "data_read isn't active but should be");
+        assert(data_address==100) else $fatal(1, "address from memory being loaded, incorrect");
+        #2;
+        /* checks register 8*/
+        instr_readdata = 32'b10101100000010000000000000000000;
+
+        assert(data_writedata==9) else $fatal(1, "incorrect value loaded to register");
+       
+    end
+
+
+    mips_cpu_harvard dut(
+        .clk(clk),
+        .reset(reset),
+        .active(active),
+        .register_v0(register_v0),
+        .clk_enable(clk_enable),
+        .instr_address(instr_address),
+        .instr_readdata(instr_readdata),
+        .data_address(data_address),
+        .data_write(data_write),
+        .data_read(data_read),
+        .data_writedata(data_writedata),
+        .data_readdata(data_readdata)
+    );
+
+endmodule
