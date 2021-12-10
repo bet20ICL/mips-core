8ea75954b12a91cf00d1e154e9650f3ab9112115
bardia01 <bardia.mzad@gmail.com>
2021-12-10 22:41:52 +0000

    qol

--- rtl/mips_cpu/alu.v
+++ rtl/mips_cpu/alu.v
@@ -2,22 +2,22 @@ module alu(
     input logic[31:0] op1, // data from rs,
     input logic[31:0] op2, // data from rt,
     input logic[31:0] instructionword,
-    output logic[31:0] result,
-    output logic[31:0] hi,
-    output logic[31:0] lo,
-    output logic[31:0] memaddroffset,
+    output logic[31:0] result,hi,lo,memaddroffset,
     output logic b_flag
-    // // zero, carry, overflow, yesbranch
-);
+); 
     logic [5:0] opcode;
     assign opcode = instructionword[31:26];
 
     logic [5:0] funct;
     assign funct = instructionword[5:0];
-    logic [31:0] ex_imm;
-    assign ex_imm = {instructionword[15] ? 16'hFFFF : 16'h0, instructionword[15:0]};
+    logic [15:0] simmediatedata, uimmediatedata;
+    assign simmediatedata = {{16{instructionword[15]}}, instructionword[15:0]};
+    assign uimmediatedata = {{16{1'b0}}, instructionword[15:0]};
     logic [5:0] shamt;
     assign shamt = instructionword[10:6];
+    
+
+    logic [31:0] result;
 
     logic signed [31:0]  sign_op1;
     assign sign_op1 = $signed(op1);
@@ -36,21 +36,18 @@ module alu(
 
     logic[63:0] multresult;
 
-    logic r_format, i_format, j_format;
     
-    always @(opcode,funct, op1,op2) begin
-        result = 0;
-        result = 0;
+
+    always @(instructionword) begin
         case(opcode)
             0:  begin
-                    r_format = 1; 
                     case(funct)
-                        0: result = unsign_op2 << shamt; // out to rd |SLL
-                        2: result = unsign_op2 >> shamt;// out to rd |SRL
-                        3: result = unsign_op2 >>> shamt;// out to rd |SRA
-                        4: result = unsign_op2 << unsign_op1;// out to rd |SLLV
-                        6: result = unsign_op2 >> unsign_op1;// out to rd |SLRV
-                        7: result = unsign_op2 >>> unsign_op1;// out to rd |SRAV
+                        0: unsigned_result = unsign_op2 << shamt; // out to rd |SLL
+                        2: unsigned_result = unsign_op2 >> shamt;// out to rd |SRL
+                        3: unsigned_result = unsign_op2 >>> shamt;// out to rd |SRA
+                        4: unsigned_result = unsign_op2 << unsign_op1;// out to rd |SLLV
+                        6: unsigned_result = unsign_op2 >> unsign_op1;// out to rd |SRLV
+                        7: unsigned_result = unsign_op2 >>> unsign_op1;// out to rd |SRAV
                         24: begin
                                 multresult = sign_op1 * sign_op2; // out to hi,lo |mult
                                 hi = multresult[63:32];
@@ -62,33 +59,34 @@ module alu(
                                 lo = multresult[31:0];
                             end 
                         26: begin // out to hi,lo |div
-                                //result = sign_op1 / sign_op2;
+                                //unsigned_result = sign_op1 / sign_op2;
                                 hi = sign_op1%sign_op2;
                                 lo = sign_op1/sign_op2;
                             end  
                         27: begin // out to hi,lo |divu
-                                //result = unsign_op1 / unsign_op2; 
+                                //unsigned_result = unsign_op1 / unsign_op2; 
                                 hi = unsign_op1%unsign_op2;
                                 lo = unsign_op1/unsign_op2;
                             end 
-                        16: result = hi;//out to rd |MFHI
+                        //16: //unsigned_result = hi;//out to rd |MFHI
                         17: hi = op1; //MTHI
-                        18: result = lo;//out to rd |MFLO
+                        //18: //unsigned_result = lo;//out to rd |MFLO
                         19: lo = op1;//MTLO
-                        32: result = sign_op1 + sign_op2;//out to rd |add
-                        33: result = unsign_op1 + unsign_op2; // out to rd |addu
-                        34: result = sign_op1 - sign_op2; //out to rd |sub
-                        35: result = unsign_op1 - unsign_op2;//out to rd |subu
-                        36: result = unsign_op1 & unsign_op2;//out to rd |and
-                        37: result = unsign_op1 | unsign_op2;//out to rd |or
-                        38: result = unsign_op1 ^ unsign_op2; // out to rd |xor
-                        39: result = ~(unsign_op1 | unsign_op2); //out to rd |nor
-                        42: result = sign_op1 < sign_op2 ? 1:0; //out to rd |set lt
-                        43: result = unsign_op1 < unsign_op2 ? 1:0;//out to rd |set ltu
+                        32: unsigned_result = sign_op1 + sign_op2;//out to rd |add
+                        33: unsigned_result = unsign_op1 + unsign_op2; // out to rd |addu
+                        34: unsigned_result = sign_op1 - sign_op2; //out to rd |sub
+                        35: unsigned_result = unsign_op1 - unsign_op2;//out to rd |subu
+                        36: unsigned_result = unsign_op1 & unsign_op2;//out to rd |and
+                        37: unsigned_result = unsign_op1 | unsign_op2;//out to rd |or
+                        38: unsigned_result = unsign_op1 ^ unsign_op2; // out to rd |xor
+                        39: unsigned_result = ~(unsign_op1 | unsign_op2); //out to rd |nor
+                        42: unsigned_result = sign_op1 < sign_op2 ? 1:0; //out to rd |set lt
+                        43: unsigned_result = unsign_op1 < unsign_op2 ? 1:0;//out to rd |set ltu
                     endcase
                 end
                 // for branches, bflag is sent out, bflag = 1 when branching is valid | 'result' output should do nothing
                 //bgez, bgezal, bltz, blez 
+                //branch and link 
             1: case(addr_rt)
                     1:if(op1>=0) begin// //bgez
                         b_flag = 1;
@@ -97,6 +95,7 @@ module alu(
                         b_flag = 0;
                     end
                     17:if(op1>=0) begin//bgezal
+                        link = 1;
                         b_flag = 1;
                     end
                     else begin
@@ -109,6 +108,7 @@ module alu(
                         b_flag = 0;
                     end
                     16:if(op1<0) begin//bltzal
+                        link = 1;
                         b_flag = 1;
                     end
                     else begin
@@ -128,7 +128,7 @@ module alu(
                 else begin
                     b_flag = 0;
                 end
-            6:  if(op1<=0) begin// blez branch less than or equal to 0-
+            6:  if(op1<=0) begin// blez 
                     b_flag = 1;
                 end
                 else begin
@@ -140,17 +140,24 @@ module alu(
                 else begin
                     b_flag = 0;
                 end
-            8: result = sign_op1 + ex_imm ;//out to rt |addi;
-            9: result = unsign_op1 + ex_imm;//out to rt |addiu
-            10: result = (sign_op1 < ex_imm) ? 1:0;//out to rt |slti
-            11: result = (unsign_op1 < ex_imm) ? 1:0; //out to rt |sltiu
-            12: result = unsign_op1 & ex_imm;//out to rt |andi
-            13: result = unsign_op1 | ex_imm;//out to rt |ori
-            14: result = unsign_op1 ^ ex_imm; //out to rt |XORI
-            15: result = ex_imm << 16;//out to rt |lui
-            //memory access instructions
-            35: memaddroffset = unsign_op1 + ex_imm;//lw
-            43: memaddroffset = unsign_op1 + ex_imm;//sw
+            8: unsigned_result = sign_op1 + simmediatedata ;//out to rt |addi
+            9: unsigned_result = unsign_op1 + simmediatedata;//out to rt |addiu
+            10: unsigned_result = (sign_op1<simmediatedata) ? 1:0;//out to rt |slti
+            11: unsigned_result = (unsign_op1<simmediatedata) ? 1:0; //out to rt |sltiu
+            12: unsigned_result = unsign_op1 & uimmediatedata;//out to rt |andi
+            13: unsigned_result = unsign_op1 | uimmediatedata;//out to rt |ori
+            14: unsigned_result = unsign_op1 ^ uimmediatedata; //out to rt |XORI
+            15: unsigned_result = uimmediatedata<<16;//out to rt |lui
+            //memory access instructions - output is a wire called memaddroffset 
+            32: memaddroffset = unsign_op1 + simmediatedata;//lb
+            33: memaddroffset = unsign_op1 + simmediatedata;//lh
+            34: memaddroffset = unsign_op1 + simmediatedata;//lw
+            36: memaddroffset = unsign_op1 + simmediatedata;//lbu
+            37: memaddroffset = unsign_op1 + simmediatedata;//lhu
+            40: memaddroffset = unsign_op1 + simmediatedata;//sb
+            41: memaddroffset = unsign_op1 + simmediatedata;//sh
+            43: memaddroffset = unsign_op1 + simmediatedata;//sw
         endcase
+        result = unsigned_result;
     end
 endmodule
