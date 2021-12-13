c6a8b37eb0622e4d248864aa59dbb0d8ec74eb76
Nicholas Mytilineos <nicholas.mytilineos@hotmail.com>
2021-12-13 22:05:41 +0000

    state_machine

--- rtl/mips_cpu/state_machine.v
+++ rtl/mips_cpu/state_machine.v
@@ -1,44 +1,60 @@
 module state_machine(
 
-    input logic     clk,
-    output logic    PC_inc,
-    output logic    IR_write,
-    output logic    alu_op,
-    output logic    mem_read,
-    output logic    mem_write,
-    output logic    reg_write,
+    input logic       clk,
+    output logic      PC_inc,
+    output logic      IR_write,
+    output logic      alu_op,
+    output logic      mem_read,
+    output logic      mem_write,
+    output logic      reg_write,
     output logic[2:0] state
 
 );
     logic[2:0]  state_next;
-    
-    always @(*) begin
-        if (state != 1 || state != 2 || state != 3 || state != 4) begin
-            state_next = 0;
-        end
-        else if (state==4) begin
-            state_next = 0;
-        end
-        else begin
-            state_next = state + 1;
-        end
+
+    initial begin
+        state=0;
+        state_next=0;
+        PC_inc = 1;
+        IR_write = 1;
+        alu_op = 0;
+        mem_read = 0;
+        mem_write = 0;
+        reg_write = 0;
     end
+    
+    always @(negedge clk) begin
+        state_next=state_next+1;
+        if (state_next==4) begin 
+            state_next = 0;
         
+        end
+        
+    end
 
     always_ff @(posedge clk) begin
         state <= state_next;
     end
+        
 
-    always_comb begin 
-        if(state==0)  
+    always_comb begin
+        
+        
+        
+    
+
+ 
+        if(state==0) begin
             PC_inc = 1;
             IR_write = 1;
             alu_op = 0;
             mem_read = 0;
             mem_write = 0;
-            reg_write = 0;        
-    
+            reg_write = 0;
         end
+        
+    
+
         if (state==1) begin 
             PC_inc = 0;
             IR_write = 0;
