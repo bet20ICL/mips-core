7519c99d1d5bd8eda5e981bdb1e03b7473c5bfda
bet20ICL <bet20@ic.ac.uk>
2021-12-15 22:21:53 +0000

    issie changes

--- test/tb/addiu_3_harvard_tb.v
+++ test/tb/addiu_3_harvard_tb.v
@@ -36,58 +36,49 @@ module addiu_tb ();
         $fatal(2, "too long");
     end
 
-    logic [31:0] test_addr;
-    initial begin
-        force_read = 1;
-        data_address = 0;
-        repeat (30) begin
-            #1;
-            $display("d_ram[%h] = %h:", data_address, data_readdata);
-            data_address += 1;
-        end
-    end
-
+    // logic [31:0] test_addr;
     // initial begin
-    //     force_read=0;
-    //     clk_enable = 1;
-    //     reset = 1;
-    //     clk_enable = 1;
-    //     @(posedge clk);
-    //     #2;
-
-    //     reset = 0;
-    //     @(posedge clk);
-    //     #2;
-
-    //     while(active) begin
-    //         @(posedge clk);
-    //         #2;
-    //     end
-    //     test = 0;
     //     force_read = 1;
-    //     #2;
-    //     $display(read);
-    //     repeat (4096) begin
-    //         $display(data_readdata);
+    //     data_address = 0;
+    //     repeat (30) begin
     //         #1;
-    //         test += 1;
-    //         #1;
+    //         $display("d_ram[%h] = %h", data_address, data_readdata);
+    //         data_address += 1;
     //     end
-
-    //     i = 2;
-    //     res_addr = 32'h00000480;
-    //     force_read = 1;
-    //     repeat(30) begin
-    //         exp_val = (16'h1111)*(i-2) + 32'h12345678 + (i-2) * 32'hdcba1234;
-    //         $display("%h, %h", data_readdata, exp_val);
-    //         assert(data_readdata==exp_val) else $fatal(1, "wrong value loaded");
-    //         i = i+1;
-    //         res_addr = res_addr+4;
-    //     end
+    //     $finish(0);
     // end
 
+    initial begin
+        force_read=0;
+        clk_enable = 1;
+        reset = 1;
+        @(posedge clk);
+        #2;
+
+        reset = 0;
+        @(posedge clk);
+        #2;
+
+        while(active) begin
+            @(posedge clk);
+            #2;
+        end
+
+        i = 2;
+        res_addr = 32'h00000480;
+        force_read = 1;
+        repeat(30) begin
+            #1;
+            exp_val = (16'h1111)*(i-2) + 32'h12345678 + (i-2) * 32'hdcba1234;
+            assert(data_readdata==exp_val) else $fatal(1, "wrong value loaded: expected=%h, got=%h", exp_val, data_readdata);
+            i = i+1;
+            res_addr = res_addr + 4;
+        end
+        $finish(0);
+    end
+
     assign read = data_read | force_read;
-    assign addr = force_read ? data_address : res_addr;
+    assign addr = force_read ? res_addr : data_address;
 
     addiu_3_dram dram(
         .clk(clk),
@@ -103,19 +94,19 @@ module addiu_tb ();
         .instr_readdata(instr_readdata)
     );
 
-    // mips_cpu_harvard dut(
-    //     .clk(clk),
-    //     .reset(reset),
-    //     .active(active),
-    //     .register_v0(register_v0),
-    //     .clk_enable(clk_enable),
-    //     .instr_address(instr_address),
-    //     .instr_readdata(instr_readdata),
-    //     .data_address(data_address),
-    //     .data_write(data_write),
-    //     .data_read(data_read),
-    //     .data_writedata(data_writedata),
-    //     .data_readdata(data_readdata)
-    // ); 
+    mips_cpu_harvard dut(
+        .clk(clk),
+        .reset(reset),
+        .active(active),
+        .register_v0(register_v0),
+        .clk_enable(clk_enable),
+        .instr_address(instr_address),
+        .instr_readdata(instr_readdata),
+        .data_address(data_address),
+        .data_write(data_write),
+        .data_read(data_read),
+        .data_writedata(data_writedata),
+        .data_readdata(data_readdata)
+    ); 
 
 endmodule
