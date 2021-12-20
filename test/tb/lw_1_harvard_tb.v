511aad950fedd5f375eb430dc6e49784f9cb502b
lucjones02 <luc.xav.jones@gmail.com>
2021-12-20 17:20:09 +0000

    done pt1

--- test/tb/lw_1_harvard_tb.v
+++ test/tb/lw_1_harvard_tb.v
@@ -1,6 +1,6 @@
-module lw_tb();
+module or_2_tb ();
 
-    logic     clk;
+    logic clk;
     logic     reset;
     logic    active;
     logic[31:0] register_v0;
@@ -19,102 +19,93 @@ module lw_tb();
     logic[31:0]  data_writedata;
     logic[31:0]  data_readdata;
 
-    /* memory initialisation */
-    logic init_mem, instr_active;
-    logic[31:0] init_mem_addr;
-    logic[31:0] init_instr;
+    logic[5:0] i;
+    logic[31:0] exp_val;
+
+    logic read, tb_read;
+    logic [31:0] addr, tb_addr;
+    logic [31:0] test;
+
+    function [31:0] reverse_endian;
+        input [31:0] a;
+        begin
+            logic [31:0] tmp;
+            tmp[7:0] = a[31:24];
+            tmp[15:8] = a[23:16];
+            tmp[23:16] = a[15:8];
+            tmp[31:24] = a[7:0];
+            reverse_endian = tmp; 
+        end
+    endfunction
 
     initial begin
         clk = 0;
         #4;
-        repeat (1000) begin
+        repeat (10000) begin
             clk = ~clk;
             #4;
-        end;
+        end
+        $fatal(2, "too long");
     end
+
+    logic [31:0] test_addr;
+    logic [31:0] undefined;
 
     initial begin
-        //instantiate variables for easier instruction building
-        logic [5:0] opcode;
-        logic [4:0] rt;
-        logic [4:0] rs;
-        logic [15:0] imm;
-        logic [31:0] imm_instr;
-        logic [31:0] ex_imm;
-        logic [15:0] test_imm;
-        
-        reset = 1;
+        tb_read=0;
         clk_enable = 1;
-
+        reset = 1;
         @(posedge clk);
         #2;
+
         reset = 0;
-
         @(posedge clk);
         #2;
 
-        //lw r2, offset r3
-        //v0 -> 9 
-        opcode = 6'b100011;
-        rs = 6'd3;
-        rt = 6'd2;
-        imm = 16'hFFFA;
-        imm_instr = {opcode, rs, rt, imm};
-
-        instr_readdata = imm_instr;
-        data_readdata = 32'd9;
-        ex_imm = {(imm[15] ? 16'hFFFF : 16'h0), imm};
-        data_address_wanted = ex_imm + 0;
+        while (active) begin
+            @(posedge clk);
+            #2;
+        end
         
-        #2;
-
-        @(posedge clk);
-        #2;
-        assert(!data_write) else $fatal(1, "data_write should not be active but is");
-        assert(data_read) else $fatal(1, "data_read isn't active but should be");
-        assert(data_address == data_address_wanted) else $fatal(1, "expected addr=%h, actual=%h", data_address_wanted, data_address);
-        assert(register_v0 == 9) else $fatal(1, "wrong value loaded");
-
-        //lw r3, offset r8
-        //r3 -> 1
-        opcode = 6'b100011;
-        rs = 6'd8;
-        rt = 6'd3;
-        imm = 32'h8;
-        imm_instr = {opcode, rs, rt, imm};
-
-        instr_readdata = imm_instr;
-        data_readdata = 32'd16;
-        ex_imm = {(imm[15] ? 16'hFFFF : 16'h0), imm};
-        data_address_wanted = ex_imm + 0;
-
-        @(posedge clk);
-        #2;
-        assert(!data_write) else $fatal(1, "data_write should not be active but is");
-        assert(data_read) else $fatal(1, "data_read isn't active but should be");
-        assert(data_address == data_address_wanted) else $fatal(1, "address from memory being loaded, incorrect");
-        assert(register_v0 == 9) else $fatal(1, "wrong value loaded");
-
-        //lw r2, offset r3
-        //r2 -> 1
-        opcode = 6'b100011;
-        rs = 6'd3;
-        rt = 6'd2;
-        imm = 32'h8;
-        imm_instr = {opcode, rs, rt, imm};
-
-        instr_readdata = imm_instr;
-        data_readdata = 32'd10;
-        ex_imm = {(imm[15] ? 16'hFFFF : 16'h0), imm};
-        data_address_wanted = ex_imm + 16;
-
-        @(posedge clk);
-        #2;
-        assert(!data_write) else $fatal(1, "data_write should not be active but is");
-        assert(data_read) else $fatal(1, "data_read isn't active but should be");
-        assert(data_address == data_address_wanted) else $fatal(1, "address from memory being loaded, incorrect");
-        assert(register_v0 == 10) else $fatal(1, "wrong value loaded");
+        tb_read = 1;
+        tb_addr = 32'h100;
+        i = 3;
+        repeat (14) begin
+            exp_val = 32'h12345678 + (i - 3) * 32'hdcba1234;
+            #1;
+            $display("mem[%h] = %h", tb_addr, reverse_endian(data_readdata));
+            #1;
+            assert(exp_val == reverse_endian(data_readdata)) else $fatal(1, "expected = %h", exp_val);
+            tb_addr += 4;
+            #1;
+            i += 1;
+        end
+        $finish(0);
     end
+
+    assign read = data_read | tb_read;
+    always_comb begin
+        if (tb_read) begin
+            addr = tb_addr;
+        end
+        else begin
+            addr = data_address;
+        end
+    end
+
+    lw_1_dram dram(
+        .clk(clk),
+        .data_address(addr),
+        .data_write(data_write),
+        .data_read(read),
+        .data_writedata(data_writedata),
+        .data_readdata(data_readdata)
+    );
+
+    lw_1_iram iram(
+        .instr_address(instr_address),
+        .instr_readdata(instr_readdata)
+    );
 
     mips_cpu_harvard dut(
         .clk(clk),
@@ -129,6 +120,6 @@ module lw_tb();
         .data_read(data_read),
         .data_writedata(data_writedata),
         .data_readdata(data_readdata)
-    );
+    ); 
 
 endmodule
