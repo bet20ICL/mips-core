04df33792c3546aa57db8211748a72bb82c7cec5
bet20ICL <bet20@ic.ac.uk>
2021-12-21 21:51:07 +0000

    added tb_outputs directory

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -eou pipefail
 
-srcs="test/tb_outputs/*"
+srcs="test/tb_outputs/*.log"
+
+set +e
+for i in ${srcs}; do
+    
+    >&2 echo "Removing ${i}"
+    rm ${i}
+
+done
+set -e
+
+srcs="test/tb_outputs/*_tb"
 
 set +e
 for i in ${srcs}; do
